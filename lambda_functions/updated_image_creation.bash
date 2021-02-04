d33f8f2ddc981fcc5ecde018ae2064269054c3cb
Kamil Czarnecki <kamoyl@outlook.com>
2021-02-04 15:51:35 +0100

    * corrected AMI creation
    * lambda OS update updated
    
    * added ami remowal

--- lambda_functions/updated_image_creation.bash
+++ lambda_functions/updated_image_creation.bash
@@ -10,26 +10,30 @@ else
   SCRIPT=$(readlink -f "$0")
 fi
 
-SCRIPTS_HOME=$(dirname "${SCRIPT}")
-
+SCRIPTS_HOME="/root/AWS_automation"
 PARAMETERS=$(echo "$@" | sed 's/\-l .//g')
+RANDOM_ID=$(cat /dev/urandom | tr -dc '0-9a-zA-Z' | fold -w 4 | head -n1)
+VAR="/root/var"
+TMP="/root/var/tmp"
+LOG="/root/var/log"
+mkdir -p ${VAR} ${TMP} ${LOG}
 
 if [ -d /root/AWS_automation ]
 then
   rm -rf /root/AWS_automation
-  git clone -b master https://github.com/kamoyl/AWS_automation.git /root/AWS_automation
+  git clone -b master https://github.com/kamoyl/AWS_automation /root/AWS_automation > /tmp/td-aws_cloning.log 2>&1
 else
-  git clone -b master https://github.com/kamoyl/AWS_automation.git /root/AWS_automation
+  git clone -b master https://github.com/kamoyl/AWS_automation /root/AWS_automation > /tmp/td-aws_cloning.log 2>&1
 fi
 
 yum check-update
 yum -y update
 sync
 
+cd /root/AWS_automation
 source /root/AWS_automation/config
-source /root/AWS_automation/config_aws
-aws_AMI_create ${LOCAL_EC2_INSTANCE} ${AWS_DEFAULT_NAME}_${RANDOM_ID}
-#aws_newest_ami
+DESCRIPTION="${AWS_DEFAULT_NAME}_automaticly_created_with_updates"
+aws_AMI_create ${LOCAL_EC2_INSTANCE} ${AWS_DEFAULT_NAME}_${RANDOM_ID} ${DESCRIPTION}
+rm -rf /root/AWS_automation/td-aws
 
 sync
-shutdown -h now
